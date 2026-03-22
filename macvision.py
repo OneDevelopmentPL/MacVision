a7d9c44be73854f4538bc029ba279ae80fa610d3
OneDevelopment <92977510+OneDevelopmentPL@users.noreply.github.com>
2026-03-22 14:33:23 +0100

    Update macvision.py

--- macvision.py
+++ macvision.py
@@ -1,10 +1,9 @@
 import cv2
 from ultralytics import YOLO
 
-# ładujemy pretrenowany model YOLOv8
+# Download models in Releases tab on github.com/OneDevelopmentPL/MacVision/releases
 model = YOLO("yolov8s.pt")
 
-# otwieramy kamerę (0 = domyślna)
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -12,28 +11,22 @@ while True:
     if not ret:
         break
 
-    # robimy predykcję
     results = model(frame)
 
-    # wynik to lista z jednym elementem
     r = results[0]
 
-    # r.boxes ma bounding boxy
     for box in r.boxes:
         x1, y1, x2, y2 = box.xyxy[0]
-        cls = int(box.cls[0])           # indeks klasy
-        label = model.names[cls]        # np. 'person'
-        conf = box.conf[0]              # pewność
+        cls = int(box.cls[0])          
+        label = model.names[cls]        
+        conf = box.conf[0]             
 
-        # rysuj prostokąt
         cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0,255,0), 2)
 
-        # tekst z nazwą + confidence
         text = f"{label} {conf:.2f}"
         cv2.putText(frame, text, (int(x1), int(y1)-10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,0), 2)
 
-    # pokaż
     cv2.imshow("Detekcja", frame)
     if cv2.waitKey(1) == ord('q'):
         break
